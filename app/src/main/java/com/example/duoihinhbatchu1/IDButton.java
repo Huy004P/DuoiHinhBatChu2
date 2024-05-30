74b8a8137babbca2f923fa96a701f6cae772fa98
Huy004P <114895245+Huy004P@users.noreply.github.com>
2024-05-30 08:06:15 +0700

    huy

---
+++ app/src/main/java/com/example/duoihinhbatchu1/IDButton.java
@@ -0,0 +1,18 @@
+package com.example.duoihinhbatchu1;
+
+public class IDButton {
+    private int idAnwser, idPick;
+
+    public IDButton(int idPick, int idAnwser) {
+        this.idAnwser = idAnwser;
+        this.idPick = idPick;
+    }
+
+    public int getIdPick() {
+        return idPick;
+    }
+
+    public int getIdAnwser() {
+        return idAnwser;
+    }
+}
